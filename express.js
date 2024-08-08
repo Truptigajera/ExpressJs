09e9b7136df8baf758a9a4318f219297a11f4277
Truptigajera <gajeratupi@gmail.com>
2024-08-08 16:33:07 +0530

    8/8/24

--- express.js
+++ express.js
@@ -2,53 +2,60 @@ const express = require('express')
 const server = express() // create server
 //const data = require('./friend.json');
 
-const fs = require('fs');
-const data = fs.readFileSync('./friend.json','utf-8');
+// const fs = require('fs');
+// const data = fs.readFileSync('./friend.json','utf-8');
 //console.log(data);
+
+const myFun = (req, res, next) => {
+  // console.log(req.query);
+  if (req.query.age >= "18") {
+    console.log('Success');
+    next();
+  } else {
+    res.json({ message: 'sorry Brother.....' })
+  }
+}
+
+//server.use(myFun); //application
+
 
 //post,get,put,patch,delete
 
 
-server.get('/', (req, res) => {
-  res.write('Hello World!');
+
+
+server.get('/',myFun, (req, res) => {
+  res.write('Welcome to Express Server');
   res.end();
 })
 
-// server.get("/",(req,res)=>{ 
-//   res.write('GET method - 1 ');
-//   res.end();
-// })
+server.get("/login",myFun,(req,res)=>{ 
+  res.write('welcome to login page');
+  res.end();
+})
 
-server.post("/",(req,res)=>{
+server.post("/", (req, res) => {
   // res.write('welcome to post Method');
   res.send('<h1>POST METHOD</h1>');
 })
 
-server.put("/",(req,res)=>{
-  res.json({message:'Hello put method called'})
-})
-
-server.patch("/", (req,res)=>{
-  res.status(400);
-  res.json({message:'Hello patch method called'});
-})
-
-server.get("/user",(req,res)=>{
-  res.json(JSON.parse(data));
-})
-
-// server.get("/login",(req,res)=>{
-//   res.write('welcome to login page');
-//   res.end();
+// server.put("/", (req, res) => {
+//   res.json({ message: 'Hello put method called' })
 // })
 
-server.get("/login",(req,res)=>{
-  res.write('welcome to login -1');
-  res.end();
-})
+// server.patch("/", (req, res) => {
+//   res.status(400);
+//   res.json({ message: 'Hello patch method called' });
+// })
 
-server.listen(5050,()=>{
+// server.get("/user", (req, res) => {
+//   res.json(JSON.parse(data));
+// })
+
+ 
+
+server.listen(5050, () => {
   console.log('server Start at http://localhost:5050');
-  
+
 });
 
