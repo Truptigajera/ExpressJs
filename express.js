6308c8527e1dc5a770c811d0a12fad2b78bb77ae
Truptigajera <gajeratupi@gmail.com>
2024-08-09 13:19:03 +0530

    9/8/24

--- express.js
+++ express.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const server = express() // create server
-//const data = require('./friend.json');
+const morgan = require('morgan');
 
-// const fs = require('fs');
-// const data = fs.readFileSync('./friend.json','utf-8');
-//console.log(data);
+server.use(morgan('dev'));
+
+//in-builtmiddleware
+server.use(express.json());
+server.use(express.urlencoded({extended:false}));
+server.use("/hello",express.static('public'))
+
 
 const myFun = (req, res, next) => {
-  // console.log(req.query);
-  if (req.query.age >= "18") {
-    console.log('Success');
-    next();
-  } else {
-    res.json({ message: 'sorry Brother.....' })
-  }
+  console.log(req.body);
+  next();
 }
 
 //server.use(myFun); //application
@@ -24,38 +23,22 @@ const myFun = (req, res, next) => {
 
 
 
-server.get('/',myFun, (req, res) => {
+server.get('/', (req, res) => {
   res.write('Welcome to Express Server');
   res.end();
-})
+});
 
 server.get("/login",myFun,(req,res)=>{ 
   res.write('welcome to login page');
   res.end();
-})
+});
 
 server.post("/", (req, res) => {
   // res.write('welcome to post Method');
   res.send('<h1>POST METHOD</h1>');
 })
 
-// server.put("/", (req, res) => {
-//   res.json({ message: 'Hello put method called' })
-// })
-
-// server.patch("/", (req, res) => {
-//   res.status(400);
-//   res.json({ message: 'Hello patch method called' });
-// })
-
-// server.get("/user", (req, res) => {
-//   res.json(JSON.parse(data));
-// })
-
- 
-
 server.listen(5050, () => {
   console.log('server Start at http://localhost:5050');
-
 });
 
