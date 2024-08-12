86e69361d553a9df8df76e9a9e0f852dfcddfac2
Truptigajera <gajeratupi@gmail.com>
2024-08-12 14:19:45 +0530

    12/8/24

--- express.js
+++ express.js
@@ -35,6 +35,33 @@ app.get("/", (req,res)=>{
     res.json(item);
   });
 
+  //Replace data- PUT
+  app.put("/user/:id",(req,res)=>{
+    let id= +req.params.id;
+    let userIndex = users.findIndex((item)=> item.id === id);
+    users.splice(userIndex,1,req.body);
+    res.json({ message: "User has been Replaced"})
+  });
+
+  //update Data - Patch
+  app.patch("/user/:id", (req,res)=>{
+    let id = +req.params.id;
+    let userIndex = users.findIndex((item)=>item.id === id);
+    let user = users[userIndex];
+    users.splice(userIndex,1,{...user, ...req.body});
+    res.json({message :"User has been successfully added"});
+  });
+
+  //Delete data- Delete
+  app.delete("/user/:id",(req,res)=>{
+    let id= +req.params.id;
+    let userIndex = users.findIndex((item)=> item.id === id);
+    users.splice(userIndex,1);
+    res.json({ message: "User Delete successfully"})
+  });
+
+  
+
   app.listen(5050,()=>{
     console.log(`server start at http://localhost:5050`);
     
