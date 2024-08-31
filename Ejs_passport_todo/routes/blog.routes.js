51d573c7ebd7cc1f8a590223000544f9e6158f72
Truptigajera <gajeratupi@gmail.com>
2024-08-31 14:16:16 +0530

    31-08-24

---
+++ Ejs_passport_todo/routes/blog.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { showBlogPage,
+    addBlog
+} = require('../controller/blog.controller');
+const blogRoutes = express.Router();
+
+blogRoutes.get("/", showBlogPage);
+blogRoutes.post("/", addBlog);
+module.exports = blogRoutes;
